cb72f25c979f17d264474ed27f3baf10adf9615b
Ekaterina <seemenkina@gmail.com>
2020-07-14 18:14:06 +0700

    add nts-kem api prototype

---
+++ ntskem.go
@@ -0,0 +1,11 @@
+package go_ntskem
+
+// GenerateKey returns a new public/private key pair
+func GenerateKey() {}
+
+// Encapsulate uses a given public key produce random key
+// and compute ciphertext encapsulating this key
+func Encapsulate() {}
+
+// Decapsulate uses a private key to decrypt a ciphertext
+func Decapsulate() {}
---
+++ ntskem_test.go
@@ -0,0 +1,15 @@
+package go_ntskem
+
+import "testing"
+
+func TestGenerateKey(t *testing.T) {
+
+}
+
+func TestEncapsulate(t *testing.T) {
+
+}
+
+func TestDecapsulate(t *testing.T) {
+
+}
